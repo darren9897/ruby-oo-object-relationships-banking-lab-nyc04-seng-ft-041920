1a206d73d9ec5ba9b1cdeafe6377f182b6dae821
Darren Koomson <darrenkoomson17@gmail.com>
2020-05-14 19:53:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
+require_all 'lib'
+
 class Transfer
-  # your code here
+  attr_reader :money, :sender, :receiver
+  attr_accessor :status, :amount
+  
+  
+  def initialize(sender, receiver, money, status = "pending")
+   @money = money
+   @receiver = receiver
+   @sender = sender
+   @status = "pending"
+   @amount = 50
+  end
+  
+    def valid?
+      if receiver.bank_account.valid? == true && sender.bank_account.valid? == true
+        true
+      else
+        false
+      end
+    end
+  
+    def interviewers
+        self.interviews.collect do |interview|
+            interview.interviewer
+        end
+    end
+  
+  
+  
+  
+  
+  
+  
+  
+  
 end
